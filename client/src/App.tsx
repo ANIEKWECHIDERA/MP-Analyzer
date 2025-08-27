2cf04e48c9562f678e77854e0a1abcee18d10c2b
ANIEKWECHIDERA <c.hideraaniekwederah73@gmail.com>
2025-08-27 10:48:58 +0100

    Chagnged folders

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import Login from "./pages/Login";
 
-function App() {
-  return <></>;
-}
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
